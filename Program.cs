1770e5b60f6a89305d64513eb7b86586d1790fe0
asantaballa <al@technowati.com>
2018-07-15 09:28:11 -0700

    first pass at JSON object

--- Program.cs
+++ Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.IO;
+using System.Collections.Generic;
+using Newtonsoft.Json;
 
 namespace twPhotoStatic2
 {
@@ -20,13 +22,37 @@ namespace twPhotoStatic2
             Console.WriteLine($"Make believe writing file");
             var fileNameOut = Config.DataDirOut + "/" + "test.txt";
             Console.WriteLine($"File: {fileNameOut}");
+
+            var testModel = new
+            {
+                p1 = "val1",
+                p2 = "val2",
+                images = new List<Object>() 
+                {
+                    new
+                    {
+                        name = "image01",
+                        file = "image01.jpg",
+                        desc = "Us at the fishing hole",
+                    },
+                    new
+                    {
+                        name = "image02",
+                        file = "image02.jpg",
+                        desc = "Us not at the fishing hole",
+                    },
+                }
+            };
+
             using (var sw = new System.IO.StreamWriter(System.IO.File.Create(fileNameOut)))
             {
-                sw.WriteLine("Test01");
-                sw.WriteLine("Test02");
+                var testModelOut = JsonConvert.SerializeObject(testModel, Formatting.Indented);
+                sw.WriteLine(testModelOut);
                 sw.Flush();
                 sw.Close();
             }
+
+
         }
     }
 }
